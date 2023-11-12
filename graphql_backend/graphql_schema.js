3477d56bcb268de924f06fd205243b21e5daa3ec
pptrgi <pptrgitonga@gmail.com>
2023-11-12 16:08:22 +0300

    Makes changes to type definitions and add subscribe's schema

--- graphql_backend/graphql_schema.js
+++ graphql_backend/graphql_schema.js
@@ -12,18 +12,23 @@ type Job {
     job_id: ID!
     alien_job_id: String!
     job_title: String!
-    job_type: String!
+    employment_type: String!
     employer_name: String!
     employer_logo: String
     employer_website: String
+    company_type: String
     apply_link: String!
     job_description: String!
-    job_qualifications: String!
-    job_requirements: String
+    job_qualifications: [String!]!
+    job_responsibilities: [String!]
     date_posted: String!
-    date_expiring: String!
+    date_expiring: String
     job_city: String!
     job_country: String!
+}
+
+type SubscribeWithEmail {
+    email: String!
 }
 
 type Query {
@@ -36,21 +41,23 @@ type Mutation {
     update_profile(fullname: String, email: String, password: String): User
     save_job(saveJobInput: SaveJobInput!): Job
     delete_job(job_id: ID!): Job
+    subscribe_with_email(email: String!): SubscribeWithEmail
 }
 
 input SaveJobInput {
     job_title: String!
-    job_type: String!
+    employment_type: String!
     alien_job_id: String!
     employer_name: String!
     employer_logo: String
     employer_website: String
+    company_type: String
     apply_link: String!
     job_description: String!
-    job_qualifications: String!
-    job_requirements: String
+    job_qualifications: [String!]!
+    job_responsibilities: [String!]
     date_posted: String!
-    date_expiring: String!
+    date_expiring: String
     job_city: String!
     job_country: String!
     user_id: ID!
