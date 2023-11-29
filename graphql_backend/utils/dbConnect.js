feb4ed9623ecd5f1c1c76b6a21ae4d3f3c286b68
pptrgi <pptrgitonga@gmail.com>
2023-11-29 17:29:10 +0300

    Delete dev data

--- graphql_backend/utils/dbConnect.js
+++ graphql_backend/utils/dbConnect.js
@@ -2,24 +2,6 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const { Pool } = pg;
-
-// export const pool = new Pool({
-//   user: `${process.env.USER_NAME}`,
-//   host: `${process.env.HOST}`,
-//   database: `${process.env.DATABASE}`,
-//   port: process.env.PORT,
-//   password: `${process.env.PASSWORD}`,
-// });
-
-// export const client = new pg.Client({
-//   user: `${process.env.USER_NAME}`,
-//   host: `${process.env.HOST}`,
-//   database: `${process.env.DATABASE}`,
-//   port: process.env.PORT,
-//   password: `${process.env.PASSWORD}`,
-// });
-
 export const client = new pg.Pool({
   connectionString: `${process.env.REN_PG_URI}`,
 });
