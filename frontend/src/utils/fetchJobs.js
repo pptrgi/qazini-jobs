88e093905a28f24cec062742ddb6255bb4769de8
pptrgi <pptrgitonga@gmail.com>
2023-11-29 18:44:19 +0300

    Limit the number of searches performed in a day

--- frontend/src/utils/fetchJobs.js
+++ frontend/src/utils/fetchJobs.js
@@ -20,11 +20,14 @@ export const jobsFetcher = async (searchText, searchCount) => {
       num_pages: 1,
     },
   };
-  // ************************************************************
-  if (searchCount <= 2) {
+
+  if (searchCount <= 1) {
+    if (searchCount == 1) {
+      const timeSearchMaxReached = new Date().getTime();
+      localStorage.setItem("timeSearchMaxReached", timeSearchMaxReached);
+    }
     try {
       const response = await axios.request(options);
-      console.log(response?.data?.data);
 
       let readyToGoJobs = [];
 
@@ -54,9 +57,6 @@ export const jobsFetcher = async (searchText, searchCount) => {
       return error?.message;
     }
   } else {
-    const timeSearchMaxReached = new Date().getTime();
-    localStorage.setItem("timeSearchMaxReached", timeSearchMaxReached);
-
     const currentTime = new Date().getTime();
 
     const readConditions = () => {
@@ -67,7 +67,8 @@ export const jobsFetcher = async (searchText, searchCount) => {
     const { storedTime } = readConditions();
 
     const noSearchPeriod = 24 * 60 * 60 * 1000; // 24hrs
-    const periodStillActive = currentTime - storedTime <= noSearchPeriod;
+    const periodStillActive =
+      currentTime - parseInt(storedTime) < noSearchPeriod;
 
     if (periodStillActive) {
       toast.error("You can only perform 3 searches in 24hrs");
@@ -75,65 +76,9 @@ export const jobsFetcher = async (searchText, searchCount) => {
     } else {
       localStorage.removeItem("searchCount");
       localStorage.removeItem("timeSearchMaxReached");
+
+      toast.error("Sorry didn't quite catch that, search again");
+      localStorage.setItem("searchCount", 0);
     }
   }
-  // ************************************************************
-
-  // // Conditional fetching
-  // let searchCount = 1;
-  // const dayTab = new Date().getTime();
-  // localStorage.setItem("sCount", searchCount);
-
-  // const readConditions = () => {
-  //   return {
-  //     cnt: localStorage.getItem("sCount"),
-  //     dTab: localStorage.getItem("dayTab"),
-  //   };
-  // };
-
-  // const { cnt, dTab } = readConditions();
-
-  // if (cnt > 2) {
-  //   localStorage.setItem("dayTab", dayTab);
-  //   const lessThanADay = dTab + 24 * 60 * 60 * 1000 - new Date().getTime() > 0;
-
-  //   if (lessThanADay) {
-  //     toast.error("Please try again in 24hrs");
-  //     return "Please try again in 24hrs";
-  //   } else {
-  //     toast.error("You can only search once in 24hrs");
-  //     return "You can only search once in 24hrs";
-  //   }
-  // }
-  // try {
-  //   const response = await axios.request(options);
-  //   console.log(response?.data?.data);
-
-  //   let readyToGoJobs = [];
-
-  //   response?.data?.data?.map((job) =>
-  //     readyToGoJobs.push({
-  //       employer_name: job.employer_name,
-  //       employer_logo: job.employer_logo,
-  //       employer_website: job.employer_website,
-  //       company_type: job.employer_company_type,
-  //       alien_job_id: job.job_id,
-  //       employment_type: job.job_employment_type,
-  //       job_title: job.job_title,
-  //       apply_link: job.job_apply_link,
-  //       job_description: job.job_description,
-  //       job_qualifications: job.job_highlights.Qualifications,
-  //       job_responsibilities: job.job_highlights.Responsibilities,
-  //       date_posted: job.job_posted_at_datetime_utc,
-  //       date_expiring: job.job_offer_expiration_datetime_utc,
-  //       job_city: job.job_city,
-  //       job_country: job.job_country,
-  //     })
-  //   );
-
-  //   return readyToGoJobs;
-  // } catch (error) {
-  //   console.log(error);
-  //   return error?.message;
-  // }
 };
