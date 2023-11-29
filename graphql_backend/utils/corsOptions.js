1651a7a88137a8732483c6fcbb01002c69626934
pptrgi <pptrgitonga@gmail.com>
2023-11-29 19:02:58 +0300

    Add the cors options

--- graphql_backend/utils/corsOptions.js
+++ graphql_backend/utils/corsOptions.js
@@ -1,1 +1,15 @@
-const allowedOrigins = [""];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://qazini-jobs.vercel.app",
+];
+
+export const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
