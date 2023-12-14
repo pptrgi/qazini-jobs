7bdc921a07fc804c698bd8d8842c1f7f910ad2b1
pptrgi <pptrgitonga@gmail.com>
2023-12-14 15:22:47 +0300

    Change the share contents and add a ring around share icons

--- frontend/src/components/jobDetailPage/TitlesSection.js
+++ frontend/src/components/jobDetailPage/TitlesSection.js
@@ -14,12 +14,13 @@ import {
   RiLinkedinFill,
   RiFacebookFill,
 } from "react-icons/ri";
+import { toast } from "react-toastify";
 
 import { formatDate } from "../../utils/dateFormatter";
 import { checkCompanyLogo } from "../../utils/checkCompanyLogo";
 import { calculateRemainingDays } from "../../utils/jobRemainingDays";
 import { JobsUserContext } from "../../context/jobsUserContext";
-import { toast } from "react-toastify";
+import { assignContentsToTitle } from "../../utils/urlContentsInTitle";
 
 const TitlesSection = ({ job }) => {
   // calculate the number of remaining days to expiry
@@ -29,19 +30,8 @@ const TitlesSection = ({ job }) => {
   const hasLogo = checkCompanyLogo(job.employer_logo);
 
   // share title contents
-  const contentsInTitle = `${job?.job_title}\n\nAt ${
-    job?.employer_name
-  }\nPosted: ${formatDate(job.date_posted)}\n${
-    job?.date_expiring
-      ? `Deadline: ${formatDate(job.date_expiring)}`
-      : "No Deadline"
-  }\n${
-    job?.date_expiring
-      ? `Remaining: ${calculateRemainingDays(job.date_expiring)} days`
-      : "Open Applications"
-  }\nEmployment type - ${job?.employment_type}\nApply link: ${
-    job?.apply_link
-  }\n\nShared with love by Qazini's founder p.Gitonga. Check us out`;
+  const contentsInTitle = assignContentsToTitle(job);
+  console.log(contentsInTitle);
 
   // handle save job
   const { user } = useContext(JobsUserContext);
@@ -173,46 +163,54 @@ const TitlesSection = ({ job }) => {
               </span>
             </div>
             <div className="flex gap-[0.75rem]">
-              <WhatsappShareButton
-                title={contentsInTitle}
-                url={"https://qazini-jobs.vercel.app"}
-              >
-                <span className="text-[1rem] text-titleColor/60 hover:text-ctaColor trans_200">
-                  <RiWhatsappFill />
-                </span>
-              </WhatsappShareButton>
-              <TwitterShareButton
-                title={contentsInTitle}
-                url={"https://qazini-jobs.vercel.app"}
-                hashtags={[
-                  "#jobsearch",
-                  "#jobs",
-                  "#buildinpublic",
-                  "#growtogether",
-                ]}
-              >
-                <span className="text-[1rem] text-titleColor/60 hover:text-ctaColor trans_200">
-                  <RiTwitterXFill />
-                </span>
-              </TwitterShareButton>
-              <LinkedinShareButton
-                title={"Job Posting"}
-                summary={contentsInTitle}
-                url={"https://qazini-jobs.vercel.app"}
-              >
-                <span className="text-[1rem] text-titleColor/60 hover:text-ctaColor trans_200">
-                  <RiLinkedinFill />
-                </span>
-              </LinkedinShareButton>
-              <FacebookShareButton
-                title={contentsInTitle}
-                url={"https://qazini-jobs.vercel.app"}
-                hashtag="#jobsearch"
-              >
-                <span className="text-[1rem] text-titleColor/60 hover:text-ctaColor trans_200">
-                  <RiFacebookFill />
-                </span>
-              </FacebookShareButton>
+              <div className="detail_page_icon">
+                <WhatsappShareButton
+                  title={contentsInTitle}
+                  url={"https://twitter.com/ptrgitonga"}
+                >
+                  <span>
+                    <RiWhatsappFill />
+                  </span>
+                </WhatsappShareButton>
+              </div>
+              <div className="detail_page_icon">
+                <TwitterShareButton
+                  title={contentsInTitle}
+                  url={"https://twitter.com/ptrgitonga"}
+                  hashtags={[
+                    "#jobsearch",
+                    "#jobs",
+                    "#buildinpublic",
+                    "#growtogether",
+                  ]}
+                >
+                  <span>
+                    <RiTwitterXFill />
+                  </span>
+                </TwitterShareButton>
+              </div>
+              <div className="detail_page_icon">
+                <LinkedinShareButton
+                  title={"Job Posting"}
+                  summary={contentsInTitle}
+                  url={"https://twitter.com/ptrgitonga"}
+                >
+                  <span>
+                    <RiLinkedinFill />
+                  </span>
+                </LinkedinShareButton>
+              </div>
+              <div className="detail_page_icon">
+                <FacebookShareButton
+                  title={contentsInTitle}
+                  url={"https://twitter.com/ptrgitonga"}
+                  hashtag="#jobsearch"
+                >
+                  <span>
+                    <RiFacebookFill />
+                  </span>
+                </FacebookShareButton>
+              </div>
             </div>
           </div>
         </div>
