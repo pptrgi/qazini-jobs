ad025c1d04efd2616ad407b13863235e50d07502
pptrgi <pptrgitonga@gmail.com>
2024-01-24 16:47:48 +0300

    Fix: Refetch user jobs on job save/unsave. Update user details and token on profile update

--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { useQuery, useApolloClient } from "@apollo/client";
+import { useContext, useEffect } from "react";
+import { useQuery } from "@apollo/client";
 import { motion } from "framer-motion";
 
 import { fadeOutVariants, pageVariants } from "../transitions/transitions";
@@ -11,11 +11,10 @@ import { toastGraphqlError } from "../utils/toastGraphqlError";
 import { noInternetHandler } from "../utils/noInternet";
 
 const Board = () => {
-  const client = useApolloClient();
   const context = useContext(JobsUserContext);
 
-  // fetch the user
-  const { loading, data } = useQuery(GET_USER_QUERY, {
+  // fetch the user's info and saved jobs
+  const { loading, data, refetch } = useQuery(GET_USER_QUERY, {
     variables: { user_id: context.user?.user_id },
     onError({ graphQLErrors, networkError }) {
       if (graphQLErrors) {
@@ -29,6 +28,11 @@ const Board = () => {
     },
   });
 
+  // refetch so the board page stays updated
+  useEffect(() => {
+    refetch();
+  }, [context.savedJobs]);
+
   return (
     <motion.div
       variants={pageVariants}
@@ -40,7 +44,6 @@ const Board = () => {
         <div className="flex_center w-full">
           <div className="w-[95%] xm360:w-[99%] md800:w-[80%]">
             <div className="flex_col gap-[1.25rem]">
-              {/* <ProfileSkeleton /> */}
               <motion.section
                 variants={fadeOutVariants}
                 className="bg-tintColor3 rounded-md px-[1rem] py-[2rem] shadow-lg xm360:px-[0.75rem]"
