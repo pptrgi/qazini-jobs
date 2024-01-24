085859e5c91d3fdc01e949b919c19922ac32ec3a
pptrgi <pptrgitonga@gmail.com>
2024-01-24 16:44:42 +0300

    Format paragraphs differently

--- frontend/src/utils/paragraphMaker.js
+++ frontend/src/utils/paragraphMaker.js
@@ -1,4 +1,6 @@
 import { LiaCheckSquareSolid } from "react-icons/lia";
+import { FiCheckSquare } from "react-icons/fi";
+import { BsCheck2Square } from "react-icons/bs";
 
 export const paragraphMaker = (paragraphsArray, origin) => {
   // function accepts an array of paragraphs and the name of the content's origin then returns individual paragraphs styled based on the desired output of that content
@@ -7,12 +9,16 @@ export const paragraphMaker = (paragraphsArray, origin) => {
     return (
       <div key={index} className="flex gap-[0.5rem] mb-[0.75rem] items-start">
         {origin === "qualifications" && (
-          <span className="text-normal">
-            <LiaCheckSquareSolid />
+          <span className="text-normal text-textColor/90">
+            <BsCheck2Square />
           </span>
         )}
-        {origin === "responsibilities" && <span>{`${index + 1}.`}</span>}
-        <p>{paragraph}</p>
+        {origin === "responsibilities" && (
+          <span className="text-textColor/90 font-semibolden">{`${
+            index + 1
+          }.`}</span>
+        )}
+        <p className="tracking-wide leading-tight">{paragraph}</p>
       </div>
     );
   });
