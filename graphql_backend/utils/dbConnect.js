347f11cfd5a39253f89a03705ed0026cda02ea9a
pptrgi <pptrgitonga@gmail.com>
2024-02-02 14:18:36 +0300

    Include max clients on a pool, and timeouts for idling and db connection establishments

--- graphql_backend/utils/dbConnect.js
+++ graphql_backend/utils/dbConnect.js
@@ -2,6 +2,12 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const client = new pg.Pool({
+export const pool = new pg.Pool({
   connectionString: `${process.env.REN_PG_URI}`,
+  max: 20,
+  idleTimeoutMillis: 60000,
+  connectionTimeoutMillis: 15000,
 });
+// set pool to contain a max of 20 active clients
+// remove a client after 1 min of idling/inactivity
+// limit time for db connection establishment to 15 seconds
