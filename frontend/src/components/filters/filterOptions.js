debd2fa367c933cc0e47c49078e68b42a84d98c6
pptrgi <pptrgitonga@gmail.com>
2024-02-02 14:38:29 +0300

    Add filter options

--- frontend/src/components/filters/filterOptions.js
+++ frontend/src/components/filters/filterOptions.js
@@ -2,5 +2,7 @@ export const filterOptions = [
   { optionName: "employment type", optionProperty: "employment_type" },
   { optionName: "location", optionProperty: "job_country" },
   { optionName: "company type", optionProperty: "company_type" },
-  { optionName: "employer name", optionProperty: "employer_name" },
+  { optionName: "employer", optionProperty: "employer_name" },
+  { optionName: "job title", optionProperty: "job_title" },
+  { optionName: "job industry", optionProperty: "company_type" },
 ];
